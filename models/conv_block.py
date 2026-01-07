fc51e094f156c59c77b5a8bd527128e349030ec5
Sarathkumar-amat <poongodinall@gmail.com>
2026-01-07 15:10:48 +0000

    started with cnn architectures

---
+++ models/conv_block.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn
+import torch.nn.functional as F
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 use_bn=False, activation="relu",pool_kernal=2,dropout=0.0):
+        super().__init__()
+        self.conv = nn.conv2d(in_channels=in_channels,out_channels=out_channels,kernal=kernal_size,padding=kernal_size//2)
+        self.use_bn=use_bn
+        if use_bn:
+            self.bn = nn.BatchNorm2D(out_channels)
+        self.activation_name = activation.lower()
+        self.pool = nn.MaxPool2d(kernal_size=pool_kernal)
+        self.use_dropout = dropout > 0.0
+        if self.use_dropout:
+            self.dropout = nn.Dropout2d(dropout)
+
+    def forward(self,x):
+        x = self.conv(x)
+        if self.use_bn: 
+            x = self.bn(cnn_out)
+        if self.activation_name == "relu":
+            x = F.relu(x)
+        elif self.activation_name == "gelu":
+            x = F.gelu(x)
+        elif self.activation_name == "silu":
+            x = F.silu(x)
+        elif self.activation_name == "mish":
+            x = F.mish(x)
+        else:
+            raise ValueError(f"Unsupported activation: {self.activation_name}")
+        self.pool(x)
+        if self.use_dropout:
+            x=self.dropout(x)
+        return x
---
+++ models/imageCNN.py
@@ -0,0 +1,45 @@
+import torch
+import nn.Module
+import torch.nn.Functional
+from conv_block import conv_block
+
+
+# kernel_size=3,
+#                  filter_strategy="constant",
+#                  activation="relu",
+#                  dense_neurons=1024):
+
+class imageCNN(nn.Module):
+    def __init__(self,input_channels,num_classes,num_blocks=5,base_filters=32,
+    kernal_size=3,filter_strategy=constant,activation=relu,dense_neurons=1024):
+
+        super().__init__()
+        self.blocks = nn.ModuleList()
+        in_ch = input_channels
+        out_ch = base_filters
+        for i in range(num_blocks):
+            self.blocks.append(conv_block
+            (input_channels,output_channels,kernal_size,
+            use_bn=True,activation=activation,
+            pool_kernal=2,dropout=0.2))
+
+            in_ch=out_ch
+            if filter_strategy == "doubling":
+                out_ch *= 2
+            elif filter_strategy == "halving":
+                out_ch = max(out_ch // 2, 1)
+
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(out_ch, num_classes)
+    def forward(self, x):
+    for block in self.blocks:
+        x = block(x)
+
+    x = self.flatten(x)
+
+    # global average pooling alternative (safer)
+    # x = x.mean(dim=(2, 3))
+
+    x = self.fc(x)
+    return x
+
